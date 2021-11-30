55b6bf03f78ffedd4fc001a75791d36ee49472f4
David Sherret <dsherret@gmail.com>
2021-11-30 17:11:45 -0500

    Other fix.

--- file_fetcher.ts
+++ file_fetcher.ts
@@ -123,7 +123,7 @@ export class FileFetcher {
     const content = await response.text();
     const headers: Record<string, string> = {};
     for (const [key, value] of response.headers) {
-      headers[key.toLowerCase(), value];
+      headers[key.toLowerCase()] = value;
     }
     this.#httpCache.set(specifier, headers, content);
     return {
