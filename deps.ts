4555c5b623ddaf3762517a355a1c85666ef8e053
Max Duval <max.duval@theguardian.com>
2023-03-16 15:50:27 +0000

    chore: bump the standard library
    the std/hash module needs to be kept at v160,
    because it exports a synchronous implementation
    of the SHA256 algorithm.

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@
 
 // std library dependencies
 
-export { ensureDir } from "https://deno.land/std@0.140.0/fs/ensure_dir.ts";
-export * as colors from "https://deno.land/std@0.140.0/fmt/colors.ts";
-export { Sha256 } from "https://deno.land/std@0.140.0/hash/sha256.ts";
+export { ensureDir } from "https://deno.land/std@0.180.0/fs/ensure_dir.ts";
+export * as colors from "https://deno.land/std@0.180.0/fmt/colors.ts";
+export { Sha256 } from "https://deno.land/std@0.160.0/hash/sha256.ts";
 export {
   dirname,
   extname,
@@ -13,11 +13,11 @@ export {
   join,
   normalize,
   sep,
-} from "https://deno.land/std@0.140.0/path/mod.ts";
+} from "https://deno.land/std@0.180.0/path/mod.ts";
 export {
   readAll,
   writeAll,
-} from "https://deno.land/std@0.140.0/streams/conversion.ts";
+} from "https://deno.land/std@0.180.0/streams/mod.ts";
 
 // type only dependencies of `deno_graph`
 
--- deps_test.ts
+++ deps_test.ts
@@ -1,4 +1,4 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-export { assertEquals } from "https://deno.land/std@0.140.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.180.0/testing/asserts.ts";
 export { createGraph } from "https://deno.land/x/deno_graph@0.26.0/mod.ts";
