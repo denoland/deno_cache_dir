041150f57f78f0fbc56a867b0afd6b2089235381
David Sherret <dsherret@gmail.com>
2024-02-12 16:46:41 -0500

    refactor: use sha2 crate

--- rs_lib/src/common.rs
+++ rs_lib/src/common.rs
@@ -46,21 +46,13 @@ pub trait DenoCacheEnv: Send + Sync + std::fmt::Debug + Clone {
   fn time_now(&self) -> SystemTime;
 }
 
-pub fn checksum(v: &[&[u8]]) -> String {
-  use ring::digest::Context;
-  use ring::digest::SHA256;
+pub fn checksum(v: &[u8]) -> String {
+  use sha2::Digest;
+  use sha2::Sha256;
 
-  let mut ctx = Context::new(&SHA256);
-  for src in v {
-    ctx.update(src.as_ref());
-  }
-  let digest = ctx.finish();
-  let out: Vec<String> = digest
-    .as_ref()
-    .iter()
-    .map(|byte| format!("{byte:02x}"))
-    .collect();
-  out.join("")
+  let mut hasher = Sha256::new();
+  hasher.update(v);
+  format!("{:x}", hasher.finalize())
 }
 
 #[cfg(test)]
@@ -69,7 +61,7 @@ mod tests {
 
   #[test]
   fn test_gen() {
-    let actual = checksum(&[b"hello world"]);
+    let actual = checksum(b"hello world");
     assert_eq!(
       actual,
       "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
