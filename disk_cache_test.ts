0141d5014a74cd7ebec8cb9c63cf1d7579db40fe
David Sherret <dsherret@gmail.com>
2024-08-10 20:59:35 -0400

    update

--- disk_cache_test.ts
+++ disk_cache_test.ts
@@ -9,15 +9,15 @@ Deno.test({
     const testCases: [string, string | Error][] = [
       [
         "http://deno.land/std/http/file_server.ts",
-        "http/deno.land/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf.bin",
+        "http/deno.land/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf",
       ],
       [
         "http://localhost:8000/std/http/file_server.ts",
-        "http/localhost_PORT8000/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf.bin",
+        "http/localhost_PORT8000/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf",
       ],
       [
         "https://deno.land/std/http/file_server.ts",
-        "https/deno.land/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf.bin",
+        "https/deno.land/d8300752800fe3f0beda9505dc1c3b5388beb1ee45afd1f1e2c9fc0866df15cf",
       ],
       ["wasm://wasm/d1c677ea", new Error(`Can't convert url`)],
       [
